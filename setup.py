92b92f30097c8e8a259e6a46123511690b3778db
Chris McDonough <chrism@plope.com>
2011-02-23 21:44:57 -0500

    dont barf if we cant read README

--- setup.py
+++ setup.py
@@ -17,8 +17,13 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = ''
+    CHANGES = ''
 
 requires = [
     'setuptools',
